613190998bd4b8901ad9e7f6a8942c7641d37b55
GStelmann <102600184+GStelmann@users.noreply.github.com>
2022-04-01 09:25:55 -0300

    Update área de triangulo.cpp

--- área de triangulo.cpp
+++ área de triangulo.cpp
@@ -12,21 +12,40 @@ int main()
     if(r==1){
     cout<<"insira a base e a altura do triângulo"<<endl;
     cin>>b>>h;
+    if(b<=0 or h<=0){
+        cout<<"este triangulo não existe"<<endl;
+        return 0;
+    }
+    else{
     a=b*h/2;
     cout<<"a area e "<<a<<endl;
+    }
     }
     else{
 if(r==2){
     cout<<"insira as medidas dos 3 lados"<<endl;
     cin>>l1>>l2>>l3;
-    if(l1>=l2+l3 or l2>=l1+l3 or l3>=l2+l1){
+    if(l1>=l2+l3 or l2>=l1+l3 or l3>=l2+l1 or l1<=0 or l2<=0 or l3<=0){
         cout<<"este triangulo não existe"<<endl;
         return 0;
     }
     else{
     p=(l1+l2+l3)/2;
     a=sqrt(p*(p-l1)*(p-l2)*(p-l3));
-    cout<<" a area do triangulo e "<<a;
+    cout<<" a area do triangulo e "<<a<<endl;
+    if(l1==l2==l3){
+        cout<<"esse triangulo e equilatero"<<endl;
+    }
+    if(l1!=l2 or l1!=l3 or l3!=l2){
+            if(l1!=l2&&l1!=l3&&l3!=l2){
+        cout<<"esse triangulo é escaleno"<<endl;
+        }
+        else{
+             cout<<"esse triangulo é isosceles"<<endl;
+    }
+
+
+    }
     }
 }
     else{cout<<"Cara, vc tem problema?"<<endl;}
